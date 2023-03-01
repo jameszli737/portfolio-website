2f76a6d057faa9de3a7b6cf5ffb04bfa464e2cc4
jameszli <112044971+jameszli737@users.noreply.github.com>
2023-02-28 21:39:46 -0600

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+namespace Li_James_HW1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IWebHostEnvironment _environment;
+        public HomeController(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult FunFacts()
+        {
+            return View();
+        }
+
+        public IActionResult Projects()
+        {
+            return View();
+        }
+
+        public IActionResult Designs()
+        {
+            return View();
+        }
+        public IActionResult Spotify()
+        {
+            return View();
+        }
+
+        public IActionResult Resume()
+        {
+            string path = _environment.WebRootPath + "/files/James_Li_Resume.pdf";
+            var stream = new FileStream(path, FileMode.Open);
+            return File(stream, "application/pdf", "James_Li_Resume.pdf");
+        }
+    }
+
+}
